41e870dc6687808399c097f2114b81c47d2c3fa0
Osman <114854402+MrChaylak@users.noreply.github.com>
2025-02-03 10:25:56 +0300

    onvif discovery devices profile data send also

--- app/services/discovery_service.py
+++ app/services/discovery_service.py
@@ -14,13 +14,29 @@ def fetch_devices():
         services = wsd.searchServices(scopes=[scope1])
 
         # Extract device IP addresses
-        ipaddresses = []
+        # # ipaddresses = []
+
+        devices = []
+
         for service in services:
             xaddrs = service.getXAddrs()
+            scopes = service.getScopes()
             if xaddrs:
                 ipaddress = re.search(r'(\d+\.\d+\.\d+\.\d+)', xaddrs[0])
                 if ipaddress:
-                    ipaddresses.append(ipaddress.group(0))
+                    ip = ipaddress.group(0)
+
+                    # Extract ONVIF profile from scopes
+                    # Extract only the profile names (Streaming, T, etc.)
+                    profile_list = [
+                        scope.split("/")[-1]  # Get last part of URL
+                        for scope in map(str, scopes)
+                        if "onvif.org/Profile/" in scope
+                    ]
+
+                    devices.append({"ip": ip, "profiles": profile_list})
+
+                    # # ipaddresses.append(ipaddress.group(0))
             #         print("START----------")
             #     print(f"IP: {ipaddress.group(0)}")
 
@@ -32,7 +48,8 @@ def fetch_devices():
         print(f'\nNumber of devices detected: {len(services)}')
 
         wsd.stop()
-        return ipaddresses
+        # # return ipaddresses
+        return devices
 
     except Exception as e:
         print(f"Error fetching devices: {e}")
