d6703967d8ddb7b2babe83d29785310d1829b80b
Osman <114854402+MrChaylak@users.noreply.github.com>
2025-02-04 14:59:37 +0300

    improve onvif error function readability

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -6,28 +6,26 @@ def display(any_list):
 def handle_onvif_error(error_message):
     """
     Handles ONVIF errors by categorizing them and returning a proper error message and code.
-
+    
     Args:
-    - error_message (str): The error message returned by the ONVIF service.
-
+        error_message (str): The error message returned by the ONVIF service.
+    
     Returns:
-    - tuple: A tuple containing the error message (dict) and an HTTP status code.
+        tuple: A tuple containing the error message (dict) and an HTTP status code.
     """
 
-    # Check if the error is related to authorization issues (invalid username/password)
-    if "Sender not Authorized" in error_message:
-        if "Invalid username or password" in error_message:
-            # Return error for invalid username/password with a 401 status code
-            return {"error": "Invalid username or password. Please check your credentials."}, 401
-        else:
-            # General authorization failure, could be other issues (e.g., network, permissions)
-            return {"error": "Authorization failure. Please check your access rights."}, 403
+    error_conditions = [
+        (["Sender not Authorized", "Invalid username or password"], 
+         {"error": "Invalid username or password. Please check your credentials."}, 401),
+        (["Sender not Authorized"], 
+         {"error": "Authorization failure. Please check your access rights."}, 403),
+        (["Profile token does not exist"], 
+         {"error": "The provided profile token does not exist. Please verify the token."}, 404)
+    ]
 
-    # Check if the error is related to an invalid profile token
-    elif "Profile token does not exist" in error_message:
-        return {"error": "The provided profile token does not exist. Please verify the token."}, 404
+    for substrings, error_resp, status in error_conditions:
+        if all(sub in error_message for sub in substrings):
+            return error_resp, status
 
-    # Handle general ONVIF errors
-    else:
-        return {"error": error_message}, 500
+    return {"error": error_message}, 500
 
